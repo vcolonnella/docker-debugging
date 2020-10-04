34cccf682a27d64b292bfa44aea937744d5aa334
Vincenzo Colonnella <vincenzocolonnella1979@gmail.com>
2020-10-04 12:49:13 +0200

    CrackMes container

---
+++ build.sh
@@ -0,0 +1,2 @@
+docker build -f Dockerfile.crackenv -t crackenv .
+docker build -f Dockerfile.crackmes -t crackmes .
---
+++ run.sh
@@ -0,0 +1,1 @@
+docker run -ti --rm --cap-add=SYS_PTRACE --security-opt seccomp=unconfined crackmes /bin/bash
